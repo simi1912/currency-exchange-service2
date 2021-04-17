52684e8df8e7c55ee1cf97d6583ec144d5c66586
simi1912 <nioasca@gmail.com>
2021-04-17 20:11:58 +0300

    Initial commit

---
+++ src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CurrencyExchangeService2Application.java
@@ -0,0 +1,13 @@
+package com.simi.microservicesv2.currencyexchangeservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeService2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/simi/microservicesv2/currencyexchangeservice2/CurrencyExchangeService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.microservicesv2.currencyexchangeservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
