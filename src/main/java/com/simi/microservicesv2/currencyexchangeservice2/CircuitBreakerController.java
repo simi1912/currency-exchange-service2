7cad91471aa37efd3961d2443633886474d0458c
simi1912 <nioasca@gmail.com>
2021-04-19 15:52:13 +0300

    Added circuit breaker fallback response.

--- src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CircuitBreakerController.java
+++ src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.simi.microservicesv2.currencyexchangeservice2;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,8 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
 
         logger.info("Sample Api call received");
