f6e48a44f221979a02cc29e956900122aec1db87
simi1912 <nioasca@gmail.com>
2021-04-19 15:34:34 +0300

    Added fallback response.

--- src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CircuitBreakerController.java
+++ src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CircuitBreakerController.java
@@ -1,14 +1,30 @@
 package com.simi.microservicesv2.currencyexchangeservice2;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
+
+        logger.info("Sample Api call received");
+
+        new RestTemplate().getForEntity("etc", null);
+
         return "sample api";
+    }
+
+    public String hardcodedResponse(Exception ex){
+        return "fallback-response";
     }
 
 }
