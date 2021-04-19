d05a36c4ad8a09d5d23e79674f0c04a0947ec180
simi1912 <nioasca@gmail.com>
2021-04-19 16:00:22 +0300

    Added bulkhead.

--- src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CircuitBreakerController.java
+++ src/main/java/com/simi/microservicesv2/currencyexchangeservice2/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.simi.microservicesv2.currencyexchangeservice2;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -17,7 +18,8 @@ public class CircuitBreakerController {
     @GetMapping("sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 //    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-    @RateLimiter(name = "default")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String sampleApi(){
 
         logger.info("Sample Api call received");
